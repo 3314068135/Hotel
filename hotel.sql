b97597787c4caa7676138573eed33e6de38398c3
lisi <lisi@qq.com>
2020-04-14 11:46:36 +0800

    第一次上传

---
+++ hotel.sql
@@ -0,0 +1,72 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 50540
+ Source Host           : localhost:3306
+ Source Schema         : hotel
+
+ Target Server Type    : MySQL
+ Target Server Version : 50540
+ File Encoding         : 65001
+
+ Date: 14/04/2020 11:08:45
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for master
+-- ----------------------------
+DROP TABLE IF EXISTS `master`;
+CREATE TABLE `master`  (
+  `gid` int(255) NOT NULL AUTO_INCREMENT COMMENT '管理员id',
+  `gname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '管理员名称',
+  `gpassword` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '管理员密码',
+  PRIMARY KEY (`gid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of master
+-- ----------------------------
+INSERT INTO `master` VALUES (1, 'admin', '123456');
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `number` int(255) NOT NULL COMMENT '订单号',
+  `rid` int(255) NOT NULL COMMENT '房间号',
+  `uname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名称',
+  `date` datetime NOT NULL COMMENT '入住时间',
+  `uphone` int(255) NOT NULL COMMENT '联系方式',
+  `rprice` double(255, 2) NOT NULL COMMENT '房间价格'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for room
+-- ----------------------------
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room`  (
+  `roomid` int(255) NOT NULL COMMENT '房间id',
+  `price` double(255, 2) NOT NULL COMMENT '房间价格',
+  `state` int(255) NOT NULL COMMENT '房间当前状态0=空房,1=已预订',
+  PRIMARY KEY (`roomid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(255) NOT NULL AUTO_INCREMENT COMMENT '普通用户id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '普通用户姓名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '普通用户密码',
+  `phone` int(255) NOT NULL COMMENT '用户手机号码',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
